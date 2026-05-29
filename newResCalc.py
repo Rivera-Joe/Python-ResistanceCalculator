f6a951e9b6abbc8ab9726a415462f007e56751e4
Joe Rivera <joe.rivera2893@gmail.com>
2026-05-29 00:22:40 -0500

    added calculation function for series

--- newResCalc.py
+++ newResCalc.py
@@ -13,20 +13,48 @@ root.minsize(width=300, height=300)
 instruct = tk.Label(root, text="\nCalculate resistance below by inputting each value in their respected boxes.\nPlease seperate each value with a comma\n" )
 instruct.grid(rowspan=2, columnspan=3)
 
+def calculate():
+    seriesString = seriesBox.get()
+    if seriesString:
+        try:
+            #create list based on comma delimiter
+            seriesValues = seriesString.split(",")
+            #change list of strings to int
+            seriesValues = list(map(int, seriesValues))
+            #add series
+            totalSeries = 0
+            for x in seriesValues:
+                totalSeries += x
+            #update total series resistance
+            setSeries(totalSeries)
+        except:
+            print("error")            
+            
+def setSeries(value):
+    resistance['series'] = value
+    getSeries()
+
+def getSeries():
+    print(resistance["series"])
 #create 2 input boxes for each set up
-def createInputBoxes():
+
     #series
-    seriesBox = tk.Entry(root)
-    seriesBox.grid(row=2, column=2, pady=20)
-    seriesBox.insert(0, "   Input Series Values")
-    seriesLabel = tk.Label(root, text="Series Values")
-    seriesLabel.grid(row=2, column=1)
-    #parallel
-    parallelBox = tk.Entry(root)
-    parallelBox.grid(row=3, column=2)
-    parallelBox.insert(0, "   Input Parallel Values")
-    parallelLabel = tk.Label(root, text="Parallel Values")
-    parallelLabel.grid(row=3, column=1 )
+seriesBox = tk.Entry(root)
+seriesBox.insert(0, "Series Values")
+seriesBox.grid(row=2, column=2, pady=20)
+seriesLabel = tk.Label(root, text="Series Values")
+seriesLabel.grid(row=2, column=1)
+#parallel
+parallelBox = tk.Entry(root)
+parallelBox.insert(0, "Parallel Values")
+parallelBox.grid(row=3, column=2)
+parallelLabel = tk.Label(root, text="Parallel Values")
+parallelLabel.grid(row=3, column=1)
+
+
+subButton = tk.Button(root, text="Calculate", command=calculate)
+subButton.grid(row=4, columnspan=2, column=0, pady=20)
+
 
 #if box empty, count as zero
 #initiate both resistance types as 0
@@ -34,5 +62,6 @@ resistance = {"series": 0, "parallel": 0, "total": 0}
 
 
 
-createInputBoxes()
+
+
 root.mainloop()
